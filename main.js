e273d986abec43276eaf372b0edb28aa80bc46b9
coyote10-12 <32jenkiandd@washk12.org>
2026-03-20 13:22:20 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-// code will be here
+function main() {
+    ask("What's 5 x 5?","25");
+}
+
+function ask(question, correct) {
+    let answer=prompt(question);
+    if (answer==correct) alert("Correct!");
+    else alert("Wrong!");
+}
+
+main();
